6c16a7b15e230728d767724a5d359999601a053f
Al1gazY <al1gazy@mail.ru>
2022-12-16 16:21:44 +0600

    first commit

---
+++ Ozinshe/Ozinshe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ozinshe
+//
+//  Created by Aligazy Kismetov on 16.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
