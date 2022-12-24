5fa08de843980997f86737b4a94a8276d022deaa
Al1gazY <al1gazy@mail.ru>
2022-12-24 11:38:15 +0600

    localizing profile vc

--- Ozinshe/Ozinshe/LanguageVC.swift
+++ Ozinshe/Ozinshe/LanguageVC.swift
@@ -6,24 +6,53 @@
 //
 
 import UIKit
+import Localize_Swift
 
-class LanguageVC: UIViewController {
+class LanguageVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var languageLabel: UILabel!
+    @IBOutlet weak var backgroundView: UIView!
+    
+    let languageArray = [["English", "en"], ["Қазақша", "kk"], ["Русский", "ru"]]
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        backgroundView.layer.cornerRadius = 32
+        backgroundView.clipsToBounds = true
+        backgroundView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        languageArray.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        
+        let label = cell?.viewWithTag(1000) as! UILabel
+        label.text = languageArray[indexPath.row][0]
+        
+        let checkImageView = cell?.viewWithTag(1001) as! UIImageView
+        
+        if Localize.currentLanguage() == languageArray[indexPath.row][1]{
+            checkImageView.image = UIImage(named: "Check")
+        } else {
+            checkImageView.image = nil
+        }
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 65
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        Localize.setCurrentLanguage(languageArray[indexPath.row][1])
+        dismiss(animated: true)
+    }
 
 }
--- Ozinshe/Ozinshe/ProfileVC.swift
+++ Ozinshe/Ozinshe/ProfileVC.swift
@@ -6,24 +6,43 @@
 //
 
 import UIKit
+import Localize_Swift
 
 class ProfileVC: UIViewController {
 
+    @IBOutlet weak var languageLabel: UILabel!
+    @IBOutlet weak var languageButton: UIButton!
+    @IBOutlet weak var myProfileLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        
     }
-    */
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        myProfileLabel.text = "MY_PROFILE".localized()
+        languageButton.setTitle("LANGUAGE".localized(), for: .normal)
+        
+        if Localize.currentLanguage() == "ru"{
+            languageLabel.text = "Русский"
+        }
+        if Localize.currentLanguage() == "kk"{
+            languageLabel.text = "Қазақша"
+        }
+        if Localize.currentLanguage() == "en"{
+            languageLabel.text = "English"
+        }
+    }
+    
+    @IBAction func languageShow(_ sender: Any) {
+        let languageVC = storyboard?.instantiateViewController(withIdentifier: "LanguageVC")
+        
+        languageVC!.modalPresentationStyle = .overFullScreen
+        present(languageVC!, animated: true)
+    }
+    
 }
