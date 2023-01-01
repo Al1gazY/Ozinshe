0ee2d9f64d3662e471145330bcb85c9f71ab2409
Al1gazY <al1gazy@mail.ru>
2023-01-02 00:00:25 +0600

    main page storyboard

---
+++ Ozinshe/Ozinshe/Controllers/MainPage/MainTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MainTableViewCell.swift
+//  Ozinshe
+//
+//  Created by Aligazy Kismetov on 01.01.2023.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
